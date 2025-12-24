93af0bf4acf81fe8c49572e5312f90b9ae269980
Eutopia <2715417602@qq.com>
2025-12-24 18:16:10 +0800

    add log color format & fix path logic)

--- get_func_definition.py
+++ get_func_definition.py
@@ -4,6 +4,33 @@ import sys
 import logging
 import argparse
 
+import colorlog
+def get_logger(level=logging.INFO):
+    # 创建logger对象
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    # 创建控制台日志处理器
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+    # 定义颜色输出格式
+    color_formatter = colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s: %(message)s',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        }
+    )
+    # 将颜色输出格式添加到控制台日志处理器
+    console_handler.setFormatter(color_formatter)
+    # 移除默认的handler
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    # 将控制台日志处理器添加到logger对象
+    logger.addHandler(console_handler)
+    return logger
 
 context.log_level = "error"
 
@@ -13,8 +40,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-logger = logging.getLogger("FuncDefinitionRetriever")
-logger.setLevel(logging.INFO)
+logger = get_logger()
 logging.getLogger("pwnlib").setLevel(logging.ERROR)
 
 class FuncDefinitionRetriever:
@@ -23,8 +49,8 @@ class FuncDefinitionRetriever:
     libs = []
 
     def __init__(self, fs_path, filename, func_name):
-        self.fs_path = fs_path
-        self.filename = filename
+        self.fs_path = os.path.abspath(fs_path)
+        self.filename = os.path.basename(filename)
         self.func_name = func_name
 
     def find_target_file(self):
